2e9e82d3a21c4afb717a201165b956d1eef758fb
dwk <dwks42@gmail.com>
2017-05-29 11:25:09 -0700

    Major refactoring.
    - Timeline has no special "latest" snapshot.
    - Use move constructors for cloning Stage.
    - Add StateSpecificEvent.

--- csplib.hpp
+++ csplib.hpp
@@ -10,6 +10,69 @@
 
 namespace csplib {
 
+class Actor {
+public:
+    typedef uint64_t ActorID;
+
+    class ActorState {
+    public:
+        virtual ~ActorState() {}
+
+        virtual ActorState *clone() const = 0;
+    };
+private:
+    ActorID m_id;
+    ActorState *m_state;
+public:
+    Actor(ActorID id, ActorState *state) : m_id(id), m_state(state) {}
+    ~Actor() { delete m_state; }
+
+    ActorID id() const { return m_id; }
+    ActorState *state() { return m_state; }
+    const ActorState *state() const { return m_state; }
+
+    Actor *clone() const {
+        return new Actor(m_id, m_state->clone());
+    }
+};
+typedef Actor::ActorID ActorID;  // bring into namespace
+
+/** Represents a list of currently active Actors. A Stage may be snapshotted,
+    so every copy of a Stage is a deep copy.
+*/
+class Stage {
+private:
+    std::map<ActorID, Actor *> m_actors;
+public:
+    Stage() {}
+    Stage(const Stage &other) {
+        for(auto it : other.m_actors) {
+            add(it.second->clone());
+        }
+    }
+    Stage(Stage &&other) {
+        m_actors = other.m_actors;
+        other.m_actors.clear();
+    }
+    ~Stage() { for(auto it : m_actors) delete it.second; }
+
+    Stage &operator = (const Stage &other) = delete;
+    Stage &operator = (Stage &&other) = default;
+
+    void add(Actor *actor) { m_actors[actor->id()] = actor; }
+    void remove(ActorID id) {
+        m_actors.erase(m_actors.find(id));  // does this work if id not present?
+    }
+    Actor *get(ActorID id) {
+        auto it = m_actors.find(id);
+        return (it != m_actors.end() ? (*it).second : nullptr);
+    }
+    const Actor *get(ActorID id) const {
+        auto it = m_actors.find(id);
+        return (it != m_actors.end() ? (*it).second : nullptr);
+    }
+};
+
 class Timestamp {
 private:
     uint64_t m_raw;
@@ -25,50 +88,6 @@ public:
 
     static Timestamp makeZero() {
         return Timestamp(0);
-    }
-};
-
-typedef uint64_t ActorID;
-
-class ActorState {
-private:
-    ActorID m_actorID;
-public:
-    virtual ~ActorState() {}
-
-    const ActorID &actorID() const { return m_actorID; }
-
-    virtual ActorState *clone() const = 0;
-};
-
-class Stage {
-private:
-    std::map<ActorID, ActorState *> m_actors;
-public:
-    void setMapping(ActorID id, ActorState *actor) { m_actors[id] = actor; }
-    void clearMapping(ActorID id) { m_actors.erase(m_actors.find(id)); }
-    ActorState *find(ActorID id) {
-        auto it = m_actors.find(id);
-        return (it != m_actors.end() ? (*it).second : nullptr);
-    }
-    const ActorState *find(ActorID id) const {
-        auto it = m_actors.find(id);
-        return (it != m_actors.end() ? (*it).second : nullptr);
-    }
-
-    Stage clone() const {
-        Stage result;
-
-        for(auto actor : m_actors) {
-            result.m_actors[actor.first] = actor.second->clone();
-        }
-
-        return std::move(result);
-    }
-
-    void destroy() {
-        for(auto actor : m_actors) delete actor.second;
-        m_actors.clear();
     }
 };
 
@@ -94,23 +113,40 @@ public:
     }
 };
 
+template <typename StateType, typename BaseEvent = Event>
+class StateSpecificEvent : public BaseEvent {
+public:
+    using BaseEvent::BaseEvent;
+
+    virtual bool apply(Stage &stage) {
+        auto target = stage.get(this->target());
+        if(!target) return false;  // target Actor does not exist
+        auto specific = dynamic_cast<StateType *>(target->state());
+        if(!specific) return false;  // Actor has wrong State type
+
+        return apply(stage, specific);
+    }
+    virtual bool apply(Stage &stage, StateType *state) = 0;
+};
+
 class CallbackEvent : public Event {
 public:
+    // the wrapped event was invoked on this actor and returned this bool
     typedef std::function<void (ActorID, bool)> Callback;
 private:
-    Event *m_event;
+    Event *m_wrapped;
     bool m_lastValue;
     bool m_first;
     Callback m_callback;
 public:
-    CallbackEvent(Event *event, Callback callback)
-        : Event(event->when(), event->target()), m_event(event),
+    CallbackEvent(Event *wrapped, Callback callback)
+        : Event(wrapped->when(), wrapped->target()), m_wrapped(wrapped),
         m_first(true), m_callback(callback) {}
 
     virtual bool apply(Stage &stage) {
-        bool value = m_event->apply(stage);
+        bool value = m_wrapped->apply(stage);
         if(m_first || value != m_lastValue) {
-            m_callback(m_event->target(), value);
+            m_callback(m_wrapped->target(), value);
         }
         m_first = false;
         m_lastValue = value;
@@ -127,23 +163,20 @@ private:
 public:
     StageSnapshot(Timestamp begin) : m_begin(begin) {}
     StageSnapshot(Timestamp begin, const Stage &stage) :
-        m_stage(stage.clone()), m_begin(begin) {}
+        m_stage(stage), m_begin(begin) {}
 
     Stage &stage() { return m_stage; }
     const Stage &stage() const { return m_stage; }
     const Timestamp &begin() const { return m_begin; }
 
-    void addEvent(Event *event) {
+    void add(Event *event) {
         // TODO: replace with more efficient version than this?
         m_events.push_back(event);
         std::stable_sort(m_events.begin(), m_events.end(),
-            [](Event *a, Event *b){ return *a < *b; });
+            [] (Event *a, Event *b) { return *a < *b; });
     }
 
-    void updateRegistryTo(const Stage &stage)
-        { m_stage.destroy(); m_stage = stage.clone(); }
-    void replaceRegistryWith(Stage &stage)
-        { m_stage.destroy(); m_stage = stage; }
+    void setStage(Stage &&stage) { m_stage = std::move(stage); }
 
     const std::vector<Event *> &events() const { return m_events; }
 };
@@ -151,43 +184,36 @@ public:
 class Timeline {
 private:
     std::vector<StageSnapshot> m_snapshots;
-    StageSnapshot m_latest;
 public:
-    Timeline() : m_latest(Timestamp::makeZero()) {
+    Timeline() {
         // First snapshot is so old, it's before everything
         m_snapshots.push_back(StageSnapshot(Timestamp::makeZero()));
     }
 
-    const StageSnapshot &latest() const { return m_latest; }
+    const StageSnapshot &latest() const { return m_snapshots.back(); }
 
-    void add(Event *event) {
+    bool add(Event *event) {
         int closestIndex = indexOf(event);
-        if(closestIndex == -1) {
-            // event is too old for us.
-            return;
-        }
+        if(closestIndex == -1) return false;  // older than oldest snapshot
 
         // insert into snapshot
-        m_snapshots[closestIndex].addEvent(event);
+        m_snapshots[closestIndex].add(event);
 
         // now we need to reassemble all future snapshots
-        auto stage = m_snapshots[closestIndex].stage().clone();
-        for(unsigned i = closestIndex+1; i < m_snapshots.size(); i ++) {
-            for(auto event : m_snapshots[i-1].events()) {
+        for(unsigned i = closestIndex; i < m_snapshots.size(); i ++) {
+            auto stage = m_snapshots[closestIndex].stage();  // deep copy
+            for(auto event : m_snapshots[i].events()) {
                 event->apply(stage);
             }
-            m_snapshots[i].updateRegistryTo(stage);
+            m_snapshots[i].setStage(std::move(stage));
         }
-        // handle last snapshot specially, because it updates the current
-        for(auto event : m_snapshots.back().events()) {
-            event->apply(stage);
-        }
-        m_latest.replaceRegistryWith(stage);
+
+        return true;
     }
 
-    void makeNewSnapshot(Timestamp now) {
+    void snapshotAt(Timestamp now) {
         // push on copy of current snapshot
-        m_snapshots.push_back(StageSnapshot(now, m_latest.stage()));
+        m_snapshots.push_back(StageSnapshot(now, latest().stage()));
     }
 
     void limitSnapshots(int count) {
@@ -199,6 +225,7 @@ public:
 private:
     int indexOf(Event *event) const {
         int size = static_cast<int>(m_snapshots.size());
+        // linear search for now
         for(int i = 0; i < size; i ++) {
             if(event->when() < m_snapshots[i].begin()
                 || event->when() == m_snapshots[i].begin()) {
--- simpletest.cpp
+++ simpletest.cpp
@@ -4,7 +4,7 @@
 
 using namespace csplib;
 
-class IntState : public ActorState {
+class IntState : public Actor::ActorState {
 public:
     int value;
     IntState(int v) : value(v) {}
@@ -17,46 +17,30 @@ public:
     CreateEvent(Timestamp when, ActorID target) : Event(when, target) {}
 
     virtual bool apply(Stage &stage) {
-        // can't create if already exists
-        if(stage.find(target())) return false;
-
-        stage.setMapping(target(), new IntState(0));
-
+        if(stage.get(target())) return false;  // abort if already exists
+        stage.add(new Actor(target(), new IntState(0)));
         return true;
     }
 };
 
-class IncrementEvent : public Event {
+class IncrementEvent : public StateSpecificEvent<IntState> {
 public:
-    IncrementEvent(Timestamp when, ActorID target) : Event(when, target) {}
+    IncrementEvent(Timestamp when, ActorID target)
+        : StateSpecificEvent<IntState>(when, target) {}
 
-    virtual bool apply(Stage &stage) {
-        auto target = stage.find(this->target());
-        // needs to exist
-        if(!target) return false;
-        // needs to be IntState
-        auto is = dynamic_cast<IntState *>(target);
-        if(!is) return false;
-
-        is->value ++;
+    virtual bool apply(Stage &, IntState *state) {
+        state->value ++;
         return true;
     }
 };
 
-class DoubleEvent : public Event {
+class DoubleEvent : public StateSpecificEvent<IntState> {
 public:
-    DoubleEvent(Timestamp when, ActorID target) : Event(when, target) {}
+    DoubleEvent(Timestamp when, ActorID target)
+        : StateSpecificEvent<IntState>(when, target) {}
 
-    virtual bool apply(Stage &stage) {
-        auto target = stage.find(this->target());
-        // needs to exist
-        if(!target) return false;
-        // needs to be IntState
-        auto is = dynamic_cast<IntState *>(target);
-        if(!is) return false;
-
-        is->value *= 2;
-
+    virtual bool apply(Stage &, IntState *state) {
+        state->value *= 2;
         return true;
     }
 };
@@ -68,22 +52,22 @@ int main() {
     timeline.add(new CreateEvent(Timestamp(1006), 101));
     timeline.add(new DoubleEvent(Timestamp(1008), 101));
 
-    auto is = dynamic_cast<const IntState *>(timeline.latest().stage().find(101));
+    auto is = dynamic_cast<const IntState *>(timeline.latest().stage().get(101)->state());
     std::cout << "Before rollback & increment: " << is->value << std::endl;
 
     // insert increment in between creation and doubling events
     timeline.add(new IncrementEvent(Timestamp(1007), 101));
 
-    is = dynamic_cast<const IntState *>(timeline.latest().stage().find(101));
+    is = dynamic_cast<const IntState *>(timeline.latest().stage().get(101)->state());
     std::cout << "After rollback & increment: " << is->value << std::endl;
 
-    timeline.makeNewSnapshot(Timestamp(1010));
-    timeline.makeNewSnapshot(Timestamp(1020));
-    timeline.makeNewSnapshot(Timestamp(1030));
+    timeline.snapshotAt(Timestamp(1010));
+    timeline.snapshotAt(Timestamp(1020));
+    timeline.snapshotAt(Timestamp(1030));
 
     timeline.add(new IncrementEvent(Timestamp(1009), 100));
 
-    is = dynamic_cast<const IntState *>(timeline.latest().stage().find(100));
+    is = dynamic_cast<const IntState *>(timeline.latest().stage().get(100)->state());
     std::cout << "After rollback & increment (100): " << is->value << std::endl;
 
     return 0;
